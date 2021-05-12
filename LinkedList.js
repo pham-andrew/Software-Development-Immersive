e4affc701fc94a2c729faab1b480a02aa9f24aa5
Andrew Pham <andrewdpham3@gmail.com>
2021-05-12 13:11:01 -0600

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -1,47 +1,44 @@
 var Node = (value) => {
-  var node = {}
-
-  node.value = value;
-  node.next = null;
-
-  return node;
+	var node = {}
+	node.value = value;
+	node.next = null;
+	return node;
 }
 
 class LinkedList {
 
-  constructor() {
-      var list = {}
-      this.head = null;
-      this.tail = null;
-      this.hasHead = false;
-  }
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.hasHead = false;
+	}
 
-    addToTail(value) {
-        if(this.hasHead){
-            this.tail.next = Node(value)
-            this.tail = this.tail.next
-        } else {
-            this.head = Node(value)
-            this.tail = this.head
-            this.hasHead = true
-        }
-    }
+	addToTail(value) {
+		if(this.hasHead){
+			this.tail.next = Node(value)
+			this.tail = this.tail.next
+		} else {
+			this.head = Node(value)
+			this.tail = this.head
+			this.hasHead = true
+		}
+	}
 
-  removeHead() {
-      var temp = this.head
-      this.head = this.head.next
-      return temp.value
-  }
+	removeHead() {
+		var temp = this.head
+		this.head = this.head.next
+		return temp.value
+	}
 
-    contains(target) {
-        var current = this.head
-        while(true){
-            if(current.value===target)
-                return true
-            if(current.next===null)
-                break
-            current = current.next
-        }
-        return false
-    }
+	contains(target) {
+		var current = this.head
+		while(true){
+			if(current.value===target)
+				return true
+			if(current.next===null)
+				break
+			current = current.next
+		}
+		return false
+	}
 }
